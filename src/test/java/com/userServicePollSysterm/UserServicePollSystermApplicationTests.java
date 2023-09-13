26b2ce84fa58155667446de1fc0eef848025dce5
saraabramovitz <sh0527685990@gmail.com>
2023-09-13 11:44:42 +0300

    third commit

---
+++ src/test/java/com/userServicePollSysterm/UserServicePollSystermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userServicePollSysterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServicePollSystermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
